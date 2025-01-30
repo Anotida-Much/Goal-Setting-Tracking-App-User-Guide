54fcaca61f0db1860abd49cf9ede45501fbc23d5
Anotida <muchinhairiano@gmail.com>
2025-01-30 20:38:33 +0200

    Transform project focus from technical guide to user-friendly guide for better accessibility

---
+++ script.js
@@ -0,0 +1,9 @@
+// Update active link on click
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", function () {
+    document
+      .querySelectorAll(".nav-link")
+      .forEach((nav) => nav.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
